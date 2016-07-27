3e7f8b4c0b281f1a5de963cb57d06613c4014d4f
nova <courtois1337@gmail.com>
2016-07-26 21:19:00 -0500

    match station stored on challonge as attachment

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static(__dirname));
 
 // LIST ALL TOURNAMENTS
 app.get('/getTournaments/', function(req, res) {
+  console.log('*************************************************************');
   console.log();
   console.log('GET TOURNAMENTS');
 
@@ -32,17 +33,16 @@ app.get('/getTournaments/', function(req, res) {
 
   if (state) {
     url += '&state=' + state;
-    console.log(state);
   }
   
   request.get(url, function(error, response, body) {
-    console.log(body);
     res.send(body);
   });
 });
 
 // LIST ALL TOURNAMENT PARTICIPANTS
 app.get('/getTournamentParticipants/', function(req, res) {
+  console.log('*************************************************************');
   console.log();
   console.log('GET TOURNAMENT PARTICIPANTS');
 
@@ -64,6 +64,7 @@ app.get('/getTournamentParticipants/', function(req, res) {
 
 // LIST ALL TOURNAMENT MATCHES
 app.get('/getMatches/', function(req, res) {
+  console.log('*************************************************************');
   console.log();
   console.log('GET TOURNAMENT MATCHES');
 
@@ -85,6 +86,7 @@ app.get('/getMatches/', function(req, res) {
 
 // UPDATE MATCH
 app.post('/postMatchResults/', function(req, res) {
+  console.log('*************************************************************');
   console.log();
   console.log('UPDATE MATCH');
   
@@ -107,6 +109,70 @@ app.post('/postMatchResults/', function(req, res) {
       "match": {
         "scores_csv": score,
         "winner_id": winner_id
+      }
+    }
+  }, function(error, response, body) {
+    res.send(body);
+  });
+
+});
+
+
+// GET MATCH STATION
+app.get('/getMatchStation/', function(req, res) {
+  console.log('*************************************************************');
+  console.log();
+  console.log('GET MATCH STATION');
+
+  var api_key        = req.query.api_key;
+  var tournament_url = req.query.tournament_url;
+  var match_id       = req.query.match_id;
+  var subdomain      = req.query.subdomain;
+
+  if (subdomain) {
+    tournament_url = subdomain + '-' + tournament_url;
+  }
+
+  var url = 'https://api.challonge.com/v1/tournaments/' + tournament_url + '/matches/' + match_id + '.json?api_key=';
+  url += api_key;
+  url += '&include_attachments=1';
+
+  request.get(url, function(error, response, body) {
+    res.send(body);
+  });
+});
+
+// UPDATE MATCH STATION
+app.post('/postMatchStation/', function(req, res) {
+  console.log('*************************************************************');
+  console.log();
+  console.log('POST MATCH STATION');
+  
+  var api_key        = req.body.data.api_key;
+  var tournament_url = req.body.data.tournament_url;
+  var match_id       = req.body.data.match_id;
+  var match_station  = req.body.data.match_station || '';
+  var station_id     = req.body.data.station_id;
+  var subdomain      = req.body.data.subdomain;
+  var get_method     = req.body.data.get_method;
+
+  if (subdomain) {
+    tournament_url = subdomain + '-' + tournament_url;
+  }
+
+  var url = 'https://api.challonge.com/v1/tournaments/' + tournament_url + '/matches/' + match_id + '/attachments';
+
+  if (station_id) {
+    url += '/' + station_id;
+  }
+
+  url += '.json?api_key=';
+  url += api_key;
+
+  request({
+    url: url, method: get_method, json: {
+      "match_attachment": {
+        "description": 'station '+match_station
       }
     }
   }, function(error, response, body) {
--- src/appCtrl.js
+++ src/appCtrl.js
@@ -56,13 +56,13 @@
       $scope.getCredentials(true);
     };
 
-    $scope.setStation = function(match_id, station) {
-      localStorageService.set(match_id, station);
-    };
+    // $scope.setStation = function(match_id, station) {
+    //   localStorageService.set(match_id, station);
+    // };
 
-    $scope.getStation = function(match_id) {
-      return localStorageService.get(match_id);
-    };
+    // $scope.getStation = function(match_id) {
+    //   return localStorageService.get(match_id);
+    // };
 
     $scope.hideCompletedMatches = function() {
       localStorageService.set('hide_completed', $scope.credentials.hide_completed);
@@ -146,7 +146,18 @@
             'id': value.match.identifier,
             'round': value.match.round
           };
-          value.match.station = $scope.getStation(value.match.id);
+
+          if (value.match.attachment_count) {
+           $scope.getMatchStation(value);
+
+            // value.match.station = station_data[0];
+            // value.match.station_id = station_data[1];
+            // $scope.getMatchStation(value);
+          }
+          else {
+            // value.match.station = '';
+          }
+          
         });
 
         $scope.is_loading = false;
@@ -158,7 +169,6 @@
 
     $scope.editMatch = function(match) {
       // there really isn't anything you can do on a match that is pending, at least now
-      console.log(match.match.state);
       if(match.match.state != 'pending') {
         $scope.currentMatch = match;
         if($scope.currentMatch.match.scores_csv) {
@@ -175,21 +185,99 @@
       var p2_score = $scope.currentMatch.match.player2_score || 0;
       var score = '' + p1_score + '-' + p2_score;
 
+      var match_station = $scope.currentMatch.match.station || '';
+
       $http.post("postMatchResults/", {
         data: {
           "api_key" : $scope.credentials.api_key,
           "subdomain" : $scope.credentials.subdomain,
           "tournament_url" : $scope.activeTournament.tournament.url,
           "match_id": $scope.currentMatch.match.id,
+          "match_station": match_station,
           "score": score,
           'winner_id': $scope.currentMatch.match.winner_id
         }
       })
       .then(
        function(response){
-         $scope.setStation($scope.currentMatch.match.id, '');
+         // $scope.setStation($scope.currentMatch.match.id, '');
          $scope.getTournamentMatches();
          $('#matchModal').modal('hide');
+       }, 
+       function(response){
+        console.log(response);
+       });
+    };
+
+    $scope.getMatchStation = function(match) {
+
+      $http.get("getMatchStation/", {
+        params: {
+          "api_key" : $scope.credentials.api_key,
+          "subdomain" : $scope.credentials.subdomain,
+          "tournament_url" : $scope.activeTournament.tournament.url,
+          "match_id": match.match.id
+        }
+      })
+      .then(
+       function(response) {
+        
+        if (!response.data.match) {
+          return;
+        }
+
+        var match_data = response.data.match;
+
+        var station = '';
+        var id = '';
+
+
+        for(var i = 0; i < match_data.attachment_count; ++i) {
+
+          if(match_data.attachments[i].match_attachment.description.substring(0,8) == 'station ') {
+
+
+            match.match.station = match_data.attachments[i].match_attachment.description.substring(8);
+
+            match.match.station_id = match_data.attachments[i].match_attachment.id;
+          }
+        }
+       }, 
+       function(response){
+        console.log(response);
+       });
+    };
+
+    $scope.updateMatchStation = function(match, station) {
+
+      var match_id = match.match.id;
+      var station_id = '';
+      var get_method = 'POST';
+
+
+      if(match.match.station_id) {
+        station_id = match.match.station_id;
+        get_method = 'PUT';
+
+        if(!station) {
+          get_method = 'DELETE';
+        }
+      }
+
+      $http.post("postMatchStation/", {
+        data: {
+          "api_key" : $scope.credentials.api_key,
+          "subdomain" : $scope.credentials.subdomain,
+          "tournament_url" : $scope.activeTournament.tournament.url,
+          "match_id": match_id,
+          "match_station": station,
+          "station_id": station_id,
+          "get_method": get_method
+        }
+      })
+      .then(
+       function(response){
+        $scope.getMatchStation(match);
        }, 
        function(response){
         alert('Error, sorry. Working on it');
