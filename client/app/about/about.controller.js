703accfa9e36104ae0f309394a4da976db842cfa
nova <courtois1337@gmail.com>
2016-09-04 22:35:34 -0500

    about / contact page

---
+++ client/app/about/about.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('runbracketsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/about/about.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('runbracketsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutCtrl'
+      });
+  });
