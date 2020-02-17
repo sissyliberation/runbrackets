d50ae9194bf8bc87849a982cf3347ffc8d527f4b
nova <courtois1337@gmail.com>
2020-02-17 11:13:20 -0600

    make static server

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const express = require('express');
-const path = require('path');
-const chalk = require('chalk');
-
-const port = process.env.PORT || 8080;
-
-const app = express();
-
-app.use(express.static(__dirname));
-
-const prodPath = '/public/';
-
-app.use(express.static(path.join(__dirname, prodPath)));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, `${prodPath}index.html`));
-});
-
-app.listen(port, () => console.log(chalk(`listening on port ${port}`)));
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
